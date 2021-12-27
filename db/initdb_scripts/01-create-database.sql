9893d2c2e2987d104af2526fa205979b5f3301c0
ferc255 <alekslootsky@gmail.com>
2021-12-27 11:46:25 +0400

    DB password

--- db/initdb_scripts/01-create-database.sql
+++ db/initdb_scripts/01-create-database.sql
@@ -1,2 +1,23 @@
+DO
+$do$
+DECLARE
+    _pass json := pg_read_file('/tmp/foosta_shared/password.json', 0, 1000);
+BEGIN
+    /* This `#>> '{}'` somehow converts json into string. */
+    EXECUTE format(
+        'CREATE ROLE foostauser LOGIN SUPERUSER PASSWORD %L',
+        _pass #>> '{}');
+    /* Logging:
+    RAISE NOTICE 'Created user with password %', _pass #>> '{}';
+    */
+END
+$do$;
+
+
+create database foostadb;
+
+
+/* Without secure password storage, it would be simply:
 create role foostauser login superuser password 'foostapassword';
 create database foostadb;
+*/
