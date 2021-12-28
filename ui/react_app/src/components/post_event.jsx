0bc92dcab5ec67f46734fd677cdf7dbc3b38a745
pyloolex <alekslootsky@gmail.com>
2021-12-28 16:06:48 +0400

    Support specifying password on the UI

--- ui/react_app/src/components/post_event.jsx
+++ ui/react_app/src/components/post_event.jsx
@@ -4,18 +4,31 @@ import PostMatch from './post_match';
 import PostTournament from './post_tournament';
 
 
+var ResponseStatus = {
+  NOT_SET: 0,
+  OK: 1,
+  ERROR: 2,
+};
+
+
 class PostEvent extends React.Component {
   constructor(props)
   {
     super(props);
 
     this.state = {
+      // Preparing payload for the POST request.
       event_type: 'match',
 
       date: '',
       teams: this.genTeamsState('match'),
+      password: '',
 
+      // Service fields.
       cachedPlayers: new Set(),
+
+      responseStatus: ResponseStatus.NOT_SET,
+      responseMessage: 'Ha!',
     };
   }
 
@@ -35,31 +48,6 @@ class PostEvent extends React.Component {
         );
       }
     );
-  }
-
-  handleSubmit = () =>
-  {
-    const requestOptions = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        event_type: this.state.event_type,
-        date: this.state.date,
-        teams: this.state.teams,
-      }),
-    };
-
-    fetch('/api/events', requestOptions).then(
-      response => response.json()).then(
-        responseJson => {
-          console.log(responseJson);
-          // TODO(ferc): Reset form only when the event has been
-          // successfully created. Otherwise, it's convenient to
-          // see a form in order to figure out where the error is.
-          this.resetForm();
-          this.cachePlayers();
-        }
-      )
   }
 
   genTeamsState = (event_type) =>
@@ -83,6 +71,7 @@ class PostEvent extends React.Component {
     this.setState({
       date: '',
       teams: this.genTeamsState(this.state.event_type),
+      // Let password remain the same.
     });
   }
 
@@ -112,41 +101,117 @@ class PostEvent extends React.Component {
     this.setState({ teams });
   }
 
+  handlePassword = (event) =>
+  {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit = () =>
+  {
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        event_type: this.state.event_type,
+        date: this.state.date,
+        teams: this.state.teams,
+        password: this.state.password,
+      }),
+    };
+
+    fetch('/api/events', requestOptions).then(this.processResponse);
+  }
+
+  processResponse = (response) =>
+  {
+    response.json().then(responseJson =>
+      {
+        if (response.status === 201)
+        {
+          this.cachePlayers();
+          this.resetForm();
+
+          this.setState({
+            responseStatus: ResponseStatus.OK,
+            responseMessage: `Event "${responseJson['id']}" has been added.`,
+          });
+        }
+        else
+        {
+          this.setState({
+            responseStatus: ResponseStatus.ERROR,
+            responseMessage: JSON.stringify(responseJson, null, 2),
+          });
+        }
+      }
+    )
+  }
+
+  drawResponseMessage = () =>
+  {
+    const responseStatus = this.state.responseStatus;
+    if (responseStatus === ResponseStatus.NOT_SET)
+    {
+      return null;
+    }
+
+    const color = responseStatus === ResponseStatus.OK ?
+          '#aaeeff' : '#ffaaaa';
+    return (
+      <pre className="post_event__response_message"
+           style={{"background-color": color}}>
+        {this.state.responseMessage}
+      </pre>
+    );
+  }
+
   render()
   {
     return (
-      <div className="post-container">
-        <div className="event-type-area">
-          <label className="label">Event type:</label>
-          <select className="select"
-                  onChange={this.handleEventType}
-                  value={this.state.event_type}
-          >
-            <option value="match">Match</option>
-            <option value="tournament">Tournament</option>
-          </select>
+      <div>
+        <div className="post-container">
+          <div className="event-type-area">
+            <label className="label">Event type:</label>
+            <select className="select"
+                    onChange={this.handleEventType}
+                    value={this.state.event_type}
+            >
+              <option value="match">Match</option>
+              <option value="tournament">Tournament</option>
+            </select>
+          </div>
+            {this.state.event_type === 'match'
+             ? <PostMatch.PostMatch date={this.state.date}
+                                      setStateDate={this.setStateDate}
+                                      teams={this.state.teams}
+                                      setStateTeams={this.setStateTeams}
+                                      cachedPlayers={this.state.cachedPlayers}
+               />
+             : <PostTournament.PostTournament date={this.state.date}
+                                              setStateDate={this.setStateDate}
+                                              teams={this.state.teams}
+                                              setStateTeams={this.setStateTeams}
+                                              cachedPlayers={
+                                                this.state.cachedPlayers}
+                                              addTeam={this.addTeam}
+               />
+            }
         </div>
-        <div className="submit-area">
+
+        <div className="post_event__password-area">
+          <label className="post_event__password-label">Password:</label>
+          <input className="post_event__password-input"
+                 value={this.state.password}
+                 onChange={this.handlePassword}
+          />
+
           <button className="submit-button"
                   onClick={this.handleSubmit}
-          >Submit</button>
+          >Submit
+          </button>
         </div>
-          {this.state.event_type === 'match'
-           ? <PostMatch.PostMatch date={this.state.date}
-                                    setStateDate={this.setStateDate}
-                                    teams={this.state.teams}
-                                    setStateTeams={this.setStateTeams}
-                                    cachedPlayers={this.state.cachedPlayers}
-             />
-           : <PostTournament.PostTournament date={this.state.date}
-                                            setStateDate={this.setStateDate}
-                                            teams={this.state.teams}
-                                            setStateTeams={this.setStateTeams}
-                                            cachedPlayers={
-                                              this.state.cachedPlayers}
-                                            addTeam={this.addTeam}
-             />
-          }
+
+        {this.drawResponseMessage()}
       </div>
     );
   };
